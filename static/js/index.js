62612372e264aca8fd959aab500efe359d29d61b
Endrio Alex Korb <116845443+Endrio-Korb@users.noreply.github.com>
2024-02-27 10:16:50 -0300

    arrumado js para trocar o tema da pagina

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,10 @@
 const chk_teme = document.getElementById('chk-dark')
+const tittle = document.querySelectorAll('.tittle')
 
 chk_teme.addEventListener('change', () => {
     document.body.classList.toggle('dark')
+    Array.from(tittle).forEach(function (tittle){
+        tittle.classList.toggle('dark')
+    })
+
 })
