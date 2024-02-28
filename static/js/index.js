9c59f28d28b7c7af92cb9af7ba9a42ce8753c046
Endrio-Korb <alex.korb@uniasselvi.com.br>
2024-02-28 10:42:38 -0300

    alterado css

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,49 @@
 const chk_teme = document.getElementById('chk-dark')
-const tittle = document.querySelectorAll('.tittle')
+// const tittle = document.querySelectorAll('.tittle')
+// const input = document.querySelectorAll('.input')
+// const btn = document.querySelectorAll('.btn')
+// const nav = document.querySelectorAll('.navbar')
 
-chk_teme.addEventListener('change', () => {
-    document.body.classList.toggle('dark')
-    Array.from(tittle).forEach(function (tittle){
-        tittle.classList.toggle('dark')
-    })
+// chk_teme.addEventListener('change', () => {
+//     // Altera o tema de fundo da página
+//     document.body.classList.toggle('light')
 
+//     // Altera o tema dos titulos
+//     Array.from(tittle).forEach(function (tittle){
+//         tittle.classList.toggle('light')
+//     })
+
+//     // Altera o tema do inputs
+//     Array.from(input).forEach(function(input){
+//         input.classList.toggle('light')
+//     })
+
+//     // Altera o tema dos botãos
+//     Array.from(btn).forEach(function(btn){
+//         btn.classList.toggle('light')
+//     })
+
+//     // Altera tema da navbar
+//     Array.from(nav).forEach(function(nav){
+//         nav.classList.toggle('light')
+//     })
+// })
+
+const chk_lab = document.getElementById('checkbox-field')
+const reservados = document.querySelector('#reservados')
+const disponiveis = document.querySelector('#disponiveis')
+
+chk_lab.addEventListener('change', () => {
+    console.log('funcionou')
+    if (reservados.classList.contains('principal')){
+        reservados.classList.remove('principal')
+    } else {
+        reservados.classList.toggle('principal')
+    }
+
+    if (disponiveis.classList.contains('principal')){
+        disponiveis.classList.remove('principal')
+    } else {
+        disponiveis.classList.toggle('principal')
+    }
 })
